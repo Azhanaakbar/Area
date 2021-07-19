a1ca349958825267e7c82f50a09381c23da227dd
Azhanaakbar <85280753+Azhanaakbar@users.noreply.github.com>
2021-07-19 12:05:01 +0530

    Add files via upload

---
+++ Area.sh
@@ -0,0 +1,10 @@
+echo "enter  side of square "
+read side
+echo "enter length of rectangle"
+read length
+echo "enter breadth of rectangle"
+read breadth
+areas=$((side*side))
+echo "area of square  = $areas"
+arearect=$((length*breadth))
+echo "area of rectangle  = $arearect"
